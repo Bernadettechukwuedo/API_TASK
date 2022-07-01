9fd54b0ec655ee9607362f96a78a7bdd02de23c6
Bernadettechukwuedo <chukwuedoogochukwubernadette@gmail.com>
2022-07-01 20:26:39 +0100

    API TASK

--- I4G002432BXI/I4G002432BXI/urls.py
+++ I4G002432BXI/I4G002432BXI/urls.py
@@ -18,5 +18,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/links/", include("links.urls"))
+    path("api/links/", include("links.urls")),
+    path('api-auth/', include('rest_framework.urls'))
+
+
 ]
